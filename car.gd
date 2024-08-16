99a1a51499e92d3e2898659ea035a3631b7e6496
Shuflduf <shuflduf@gmail.com>
2024-08-15 19:42:40 -0600

    friction

--- car.gd
+++ car.gd
@@ -16,33 +16,40 @@ func _process(_delta: float) -> void:
 func rotate_forwards(dir: Vector3) -> Vector3:
 	return dir.rotated(Vector3.UP, rotation.y)
 
-func _physics_process(_delta: float) -> void:
+#func _physics_process(_delta: float) -> void:
+#
+	#engine_force = Input.get_axis("deccelerate", "accelerate") * 100
+#
+	#var dir = Input.get_axis("right", "left")
+	#if dir:
+		#if dir > 0.1:
+			#turn(Direction.LEFT, 1)
+		#elif dir < -0.1:
+			#turn(Direction.RIGHT, 1)
+	#else:
+		#for wheel in wheels:
+			#wheel.engine_force = 0
 
-	engine_force = Input.get_axis("deccelerate", "accelerate") * 100
-
-	var dir = Input.get_axis("right", "left")
-	if dir:
-		if dir > 0.1:
-			turn(Direction.LEFT, 1)
-		elif dir < -0.1:
-			turn(Direction.RIGHT, 1)
-	else:
-		for wheel in wheels:
-			wheel.engine_force = 0
-
-<<<<<<< HEAD
-	var accel = Input.get_axis("deccelerate", "accelerate")
-
-	engine_force = accel * max_torque * (1.0 / max_rpm)
-=======
-
-func turn(dir: Direction, strength: int):
-	for wheel in wheels:
-		match wheel.desired_direction:
-			Direction.NONE:
-				pass
-			dir:
-				wheel.engine_force = -strength * 50
-			_:
-				wheel.engine_force = strength * 50
->>>>>>> parent of 8d5be6d (better, but still bad car movement)
+#<<<<<<< HEAD
+#<<<<<<< HEAD
+	#var accel = Input.get_axis("deccelerate", "accelerate")
+#
+	#engine_force = accel * max_torque * (1.0 / max_rpm)
+#=======
+#
+#=======
+#
+#>>>>>>> parent of 8d5be6d (better, but still bad car movement)
+#func turn(dir: Direction, strength: int):
+	#for wheel in wheels:
+		#match wheel.desired_direction:
+			#Direction.NONE:
+				#pass
+			#dir:
+				#wheel.engine_force = -strength * 50
+			#_:
+				#wheel.engine_force = strength * 50
+#<<<<<<< HEAD
+#>>>>>>> parent of 8d5be6d (better, but still bad car movement)
+#=======
+#>>>>>>> parent of 8d5be6d (better, but still bad car movement)
--- new_car.gd
+++ new_car.gd
@@ -9,12 +9,27 @@ func _process(_delta: float) -> void:
 func _physics_process(delta: float) -> void:
 	velocity.y += delta * get_gravity().y
 	var accel = Input.get_axis("accelerate", "deccelerate") * 0.1
-	velocity += rf(Vector3(0, 0, accel))
+	var accel_dir = rf(Vector3(0, 0, accel))
+	if accel_dir.z < velocity.z:
+		accel_dir *= 0.5
+	velocity += accel_dir
 
-	velocity.z *= 0.99
+	if !accel:
+		velocity.z *= 0.95
 	print(velocity)
 
-	rotation.y += Input.get_axis("right", "left") * delta
+	var dir = Input.get_axis("right", "left")
+	rotation.y += dir * delta
+
+	if dir > 0.1:
+		$AnimatedSprite3D.play("side")
+		$AnimatedSprite3D.flip_h = false
+	elif dir < -0.1:
+		$AnimatedSprite3D.play("side")
+		$AnimatedSprite3D.flip_h = true
+	else:
+		$AnimatedSprite3D.play("default")
+		$AnimatedSprite3D.flip_h = false
 
 	move_and_slide()
 
